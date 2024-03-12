3e289f62de975ac6975dd6e7625f2c8a857e69b4
Camilo Andrés Guevara Triana <c.guevarat@uniandes.edu.co>
2024-03-12 02:16:38 +0000

    update Saga

--- src/propiedadesalpes/modulos/sagas/aplicacion/coordinadores/saga_propiedad.py
+++ src/propiedadesalpes/modulos/sagas/aplicacion/coordinadores/saga_propiedad.py
@@ -1,8 +1,11 @@
 
+from company.modules.company.application.commands.create_company import CreateCompany
+from company.modules.company.domain.events import CompanyCreated
 from modulos.propiedades.aplicacion.comandos.crear_propiedad import CrearPropiedad
 from modulos.propiedades.dominio.eventos import CreacionPropiedadFallida, PropiedadCreada
 from modulos.propiedades.infraestructura.repositorios import RepositorioSagaLog
 from modulos.sagas.aplicacion.commandos.sales import CreateSaleFailed
+from modulos.sagas.aplicacion.commandos.company import CreateCompanyFailed
 from sales.modules.sales.domain.events import PropertySold, SaleCreated
 from seedwork.aplicacion.sagas import *
 
@@ -13,7 +16,8 @@ class CoordinadorPropiedades(CoordinadorOrquestacion):
             Inicio(index=0),
             Transaccion(index=1, comando=CrearPropiedad, evento=PropiedadCreada, error=CreacionPropiedadFallida, compensacion=None),
             Transaccion(index=2, comando=SaleCreated, evento=PropertySold, error=CreateSaleFailed, compensacion=None),
-            Fin(index=2)
+            Transaccion(index=3, comando=CreateCompany, evento=CompanyCreated, error=CreateCompanyFailed, compensacion=None),
+            Fin(index=4)
         ]
     def iniciar(self):
         self.persistir_en_saga_log(self.pasos[0])
